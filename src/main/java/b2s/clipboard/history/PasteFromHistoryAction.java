256cc124ea7ded70b75f05fe28ce672019aa19a4
born2snipe <born2snipe@gmail.com>
2010-07-22 17:02:35 -0500

    renamed to history clipboard

--- src/main/java/b2s/clipboard/history/PasteFromHistoryAction.java
+++ src/main/java/b2s/clipboard/history/PasteFromHistoryAction.java
@@ -11,7 +11,7 @@
  * See the License for the specific language governing permissions and limitations under the License.
  */
 
-package b2s.clipboard.multi;
+package b2s.clipboard.history;
 
 import java.awt.datatransfer.StringSelection;
 import java.awt.event.ActionEvent;
--- src/test/java/b2s/clipboard/history/ClipboardHistoryTest.java
+++ src/test/java/b2s/clipboard/history/ClipboardHistoryTest.java
@@ -11,7 +11,7 @@
  * See the License for the specific language governing permissions and limitations under the License.
  */
 
-package b2s.clipboard.multi;
+package b2s.clipboard.history;
 
 import java.util.Arrays;
 import static junit.framework.Assert.*;
